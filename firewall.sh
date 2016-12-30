41ae899b2eaea2a3c22117baaec2b4f2b9c2e7dc
shenfu1991 <xuanyuantiandipan@163.com>
2016-12-30 17:01:27 +0800

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -1,3 +1,26 @@
+#! /bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+#===============================================================================================
+#   System Required:  CentOS, Debian, Ubuntu
+#   Description: One click Install Shadowsocks-go server
+#   Author: Teddysun <i@teddysun.com>
+#   Thanks: @cyfdecyf <https://twitter.com/cyfdecyf>
+#   Intro:  http://teddysun.com/392.html
+#===============================================================================================
+
+clear
+echo ""
+echo "#############################################################"
+echo "# One click Install Shadowsocks-go server                   #"
+echo "# Intro: http://teddysun.com/392.html                       #"
+echo "# Author: Teddysun <i@teddysun.com>                         #"
+echo "# Thanks: @cyfdecyf <https://twitter.com/cyfdecyf>          #"
+echo "#############################################################"
+echo ""
+
+# Make sure only root can run our script
+function rootness(){
                 iptables -I INPUT -m state --state NEW -m udp -p udp --dport 9999 -j ACCEPT
                 iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 8888 -j ACCEPT
                 iptables -I INPUT -m state --state NEW -m udp -p udp --dport 8888 -j ACCEPT
@@ -15,3 +38,9 @@
                 iptables -I INPUT -m state --state NEW -m udp -p udp --dport 2222 -j ACCEPT
                 iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 1111 -j ACCEPT
                 iptables -I INPUT -m state --state NEW -m udp -p udp --dport 1111 -j ACCEPT
+          echo "complete"
+
+}             
+             
+             
+               
