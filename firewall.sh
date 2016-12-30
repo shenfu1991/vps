2a52bec5a59566dcc1a0e039a052cc0c3a6030d1
shenfu1991 <xuanyuantiandipan@163.com>
2016-12-30 17:09:25 +0800

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -19,27 +19,35 @@ echo "# Thanks: @cyfdecyf <https://twitter.com/cyfdecyf>          #"
 echo "#############################################################"
 echo ""
 
-# Make sure only root can run our script
-function rootness(){
-                iptables -I INPUT -m state --state NEW -m udp -p udp --dport 9999 -j ACCEPT
-                iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 8888 -j ACCEPT
-                iptables -I INPUT -m state --state NEW -m udp -p udp --dport 8888 -j ACCEPT
-                iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 7777 -j ACCEPT
-                iptables -I INPUT -m state --state NEW -m udp -p udp --dport 7777 -j ACCEPT
-                iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 6666 -j ACCEPT
-                iptables -I INPUT -m state --state NEW -m udp -p udp --dport 6666 -j ACCEPT
-                iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 5555 -j ACCEPT
-                iptables -I INPUT -m state --state NEW -m udp -p udp --dport 5555 -j ACCEPT
-                iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 4444 -j ACCEPT
-                iptables -I INPUT -m state --state NEW -m udp -p udp --dport 4444 -j ACCEPT
-                iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 3333 -j ACCEPT
-                iptables -I INPUT -m state --state NEW -m udp -p udp --dport 3333 -j ACCEPT
-                iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 2222 -j ACCEPT
-                iptables -I INPUT -m state --state NEW -m udp -p udp --dport 2222 -j ACCEPT
-                iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 1111 -j ACCEPT
-                iptables -I INPUT -m state --state NEW -m udp -p udp --dport 1111 -j ACCEPT
-          echo "complete"
+function check_datetime(){
+rm -rf /etc/localtime
+ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+ntpdate 1.cn.pool.ntp.org
+}
 
+# Make sure only root can run our script
+function update_firewall(){
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 9999 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 9999 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 8888 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 8888 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 7777 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 7777 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 6666 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 6666 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 5555 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 5555 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 4444 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 4444 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 3333 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 3333 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 2222 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 2222 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 1111 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 1111 -j ACCEPT
+
+echo "complete"
+exit 1
 }             
              
              
